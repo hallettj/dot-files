8665d8c9120848345620487423dab3b537704bba
Jesse Hallett <jesse@sitr.us>
2014-02-11 19:25:53 -0800

    Updates auto-away script to change status based on xscreensaver state

--- home/.irssi/scripts/away_on_xscreensaver.pl
+++ home/.irssi/scripts/away_on_xscreensaver.pl
@@ -1,6 +1,6 @@
 # /AUTOAWAY <n> - Mark user away after <n> seconds of inactivity
 # /AWAY - play nice with autoaway
-# Automatically marks user as away when X server is idle
+# Automatically marks user as away when xscreensaver is active
 # (c) 2000 Larry Daffner (vizzie@airmail.net)
 #     You may freely use, modify and distribute this script, as long as
 #      1) you leave this notice intact
@@ -11,27 +11,25 @@
 
 # This is an adaptation of the auto_away.pl script by Larry Daffner.
 # This version automatically sets the user's away status based on
-# X server idle time, as reported by the xprintidle command.
+# whether the screen is blanked by xscreensaver.
 #
-# To install xprintidle on Debian install the package, xprintidle
-#
-# /autoaway <n> will mark you as away automatically if you have not used
-# the mouse or keyboard in <n> seconds. (<n>=0 disables the feature) It
-# will also automatically unmark you away the next time you type
-# a command, move the mouse, or press a key.
+# /autoaway <message> will mark you as away automatically when the
+# screen blanks.  It will also automatically unmark you away the next
+# time you type a command or unlock the screen.  Use /autoaway with no
+# arguments to disable.
 #
 # Note that using the /away command will disable the autoaway mechanism,
 # as well as the autoreturn. (when you unmark yourself, the autoaway wil
 # restart again)
 #
-# The script currently checks idle time by polling on a 30 second
-# interval.  So there will be a delay after resuming activity before you
-# are unmarked as away.  But typing a command in irssi will unmark you
-# immediately.
+# The script currently checks xscreensaver state by polling on a 10
+# second interval.  So there will be a delay after resuming activity
+# before you are unmarked as away.  But typing a command in irssi will
+# unmark you immediately.
 
 # Thanks to Adam Monsen for multiserver and config file fix
 #
-# X server idle time adaptation by Jesse Hallett
+# xscreensaver state adaptation by Jesse Hallett
 
 use Irssi;
 use Irssi::Irc;
@@ -42,16 +40,15 @@ $VERSION = "0.3";
     authors => 'Larry "Vizzie" Daffner, Jesse Hallett',
     contact => 'jesse@sitr.us',
     name => 'Away on idle',
-    description => 'Automatically goes away after defined inactivity, determined by X server idle time',
+    description => 'Automatically goes away when xscreensaver blanks the screen',
     license => 'BSD',
     url => 'http://www.flamingpackets.net/~vizzie/irssi/',
     changed => '2014-01-31',
     changes => 'Uses X server idle time to determine inactivity'
 );
 
-my $autoaway_sec;
 my $autoaway_to_tag;
-my $autoaway_poll_interval = 30;  # 30 seconds
+my $autoaway_poll_interval = 10;  # 10 seconds
 my $away_state;     # false
 my $set_manually;   # false
 my $changing_state; # false
@@ -61,29 +58,24 @@ my $message;
 # /AUTOAWAY - set the autoaway timeout
 #
 sub cmd_autoaway {
-  my ($data, $server, $channel) = @_;
-  my @params = split " ", $data;
-  my $timeout = shift @params;
+  my ($msg, $server, $channel) = @_;
+  $msg =~ s/^\s+|\s+$//g;  # Trim whitespace
 
-  $message = join " ", @params;
-  $message =~ s/^\s+|\s+$//g;  # Trim whitespace
-
-  if (!($timeout =~ /^[0-9]+/)) {
-    Irssi::print("autoaway: usage: /autoaway <seconds> [<message>]");
+  if (system("which xscreensaver-command >/dev/null")) {
+    Irssi::print("Could not locate the xscreensaver-command executable. Please install xscreensaver to use this utility.");
     return 1;
   }
 
-  if (system("which xprintidle >/dev/null")) {
-    Irssi::print("Could not locate the executable, xprintidle. Please install this utility.");
-    return 1;
+  if (system("xscreensaver-command -time 2>&1 >/dev/null")) {
+    Irssi::print("It appears that xscreensaver is not running. This utility will not work without xscreensaver.")
   }
 
-  $autoaway_sec = $timeout;
-
-  if ($autoaway_sec) {
-    Irssi::print("autoaway timeout set to $autoaway_sec seconds");
-  } else {
-    Irssi::print("autoway disabled");
+  if ($msg) {
+    Irssi::print("autoaway enabled");
+    $message = $msg;
+  }
+  else {
+    Irssi::print("autoaway disabled");
   }
 
   if (defined($autoaway_to_tag)) {
@@ -91,9 +83,9 @@ sub cmd_autoaway {
     $autoaway_to_tag = undef;
   }
 
-  if ($autoaway_sec) {
+  if ($msg) {
     $autoaway_to_tag =
-      Irssi::timeout_add($autoaway_poll_interval*1000, "check_idle_time", undef);
+      Irssi::timeout_add($autoaway_poll_interval*1000, "check_idle", undef);
   }
 }
 
@@ -101,6 +93,10 @@ sub cmd_autoaway {
 # away = Set us away or back, within the autoaway system
 sub cmd_away {
   my ($data, $server, $channel) = @_;
+
+  if ($changing_state) {
+    return 1;
+  }
 
   $set_manually = 1;
 
@@ -112,13 +108,13 @@ sub cmd_away {
   }
 }
 
-sub check_idle_time {
-  my $idle_time = `xprintidle` / 1000;
+sub check_idle {
+  my $idle = !system("xscreensaver-command -time 2>/dev/null | grep -Eq ' blanked|locked'");
 
-  if ($idle_time >= $autoaway_sec) {
+  if ($idle) {
     auto_timeout();
   }
-  elsif ($idle_time < $autoaway_sec) {
+  else {
     auto_resume();
   }
 }
@@ -128,15 +124,10 @@ sub auto_timeout {
     return 1;
   }
 
-  my $msg = $message || "autoaway after $autoaway_sec seconds";
-  if (!defined($msg)) {
-    $msg = "autoaway after $autoaway_sec seconds";
-  }
-
   # we're in the process.. don't touch anything.
   $changing_state = 1;
   foreach my $server (Irssi::servers()) {
-      $server->command("/AWAY -one $msg");
+      $server->command("/AWAY -one $message");
   }
   $changing_state = 0;
 
@@ -159,15 +150,13 @@ sub auto_resume {
   $set_manually = 0;
 }
 
-Irssi::settings_add_int("misc", "autoaway_timeout", 0);
 Irssi::settings_add_str("misc", "autoaway_message", "");
 
-my $autoaway_default = Irssi::settings_get_int("autoaway_timeout");
 $message = Irssi::settings_get_str("autoaway_message");
-if ($autoaway_default) {
-  cmd_autoaway($autoaway_default);
+if ($message) {
+  cmd_autoaway($message);
 }
 
 Irssi::command_bind('autoaway', 'cmd_autoaway');
 Irssi::command_bind('away', 'cmd_away');
-Irssi::signal_add('send command', 'auto_resume');
+# Irssi::signal_add('send command', 'auto_resume');
