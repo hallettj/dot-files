c9c470846ca7c0a588c0381aba652b47967cd7bf
Jesse Hallett <jesse@sitr.us>
2015-02-01 23:45:31 -0800

    Tweak in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo apt-get install ansible curl git myrepos
+sudo apt-get install -y ansible curl git myrepos
 mr bootstrap https://raw.githubusercontent.com/hallettj/dot-files/master/home/.mrconfig $HOME
